459af0e52385e85efd3d9ffe1958133890299c88
Tomi Laakso <tomi.j.laakso@gmail.com>
2018-11-11 17:58:24 +0200

    Filter reducer. No actions implemented

---
+++ redux-anecdotes-v2/src/reducers/filterReducer.js
@@ -0,0 +1,10 @@
+const initialState=''
+
+const filterReducer = (store = initialState, action) => {
+  console.log('FILTER ACTION: ', action)
+  switch(action.type) {
+  default:
+    return store
+  }
+}
+export default filterReducer
