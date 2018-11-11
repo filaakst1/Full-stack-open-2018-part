58356e73b0a67bdd5c38075d5ec195fa5a73bb77
Tomi Laakso <tomi.j.laakso@gmail.com>
2018-11-11 20:32:43 +0200

    Set to load initial anecdotes from backend

--- redux-anecdotes-v2/src/components/AnecdoteList.js
+++ redux-anecdotes-v2/src/components/AnecdoteList.js
@@ -1,10 +1,16 @@
 import React from 'react'
 import Filter from './Filter'
 import { connect } from 'react-redux'
-import { voteAnecdote } from '../reducers/anecdoteReducer'
+import { voteAnecdote,anecdoteInitialization } from '../reducers/anecdoteReducer'
 import { voteNotification, deleteNotification } from '../reducers/notificationReducer'
-
+import anecdoteService from '../services/anecdotes'
 class AnecdoteList extends React.Component {
+  componentDidMount = async () => {
+
+    const anecdotes =await anecdoteService.getAll()
+    this.props.anecdoteInitialization(anecdotes)
+  }
+
   voteAnecdote = (id) => (e) => {
     console.log('Vote click')
     const anecdote = this.props.anecdotesToShow.find(a => a.id === id)
@@ -38,8 +44,9 @@ class AnecdoteList extends React.Component {
     )
   }
 }
-const anecdotesToShow =(anacdotes, filter) => {
-  return anacdotes
+const anecdotesToShow =(anecdotes, filter) => {
+  console.log('#DEBUG: ',anecdotes)
+  return anecdotes
     .filter(a => filter.length === 0 || a.content.includes(filter) )
     .sort((a, b) => b.votes - a.votes)
 }
@@ -48,4 +55,4 @@ const mapStateToProps = (state) => {
     anecdotesToShow: anecdotesToShow(state.anecdotes,state.filter)
   }
 }
-export default connect(mapStateToProps,{ voteAnecdote, voteNotification, deleteNotification })(AnecdoteList)
+export default connect(mapStateToProps,{ voteAnecdote, voteNotification, deleteNotification ,anecdoteInitialization })(AnecdoteList)
--- redux-anecdotes-v2/src/reducers/anecdoteReducer.js
+++ redux-anecdotes-v2/src/reducers/anecdoteReducer.js
@@ -1,25 +1,7 @@
-const anecdotesAtStart = [
-  'If it hurts, do it more often',
-  'Adding manpower to a late software project makes it later!',
-  'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
-  'Premature optimization is the root of all evil.',
-  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
-]
 
 const getId = () => (100000*Math.random()).toFixed(0)
 
-const asObject = (anecdote) => {
-  return {
-    content: anecdote,
-    id: getId(),
-    votes: 0
-  }
-}
-
-const initialState = anecdotesAtStart.map(asObject)
-
-const anecdoteReducer = (store = initialState, action) => {
+const anecdoteReducer = (store = [], action) => {
   console.log('ANECDOTE ACTION: ', action)
   switch(action.type) {
   case 'VOTE': {
@@ -31,11 +13,19 @@ const anecdoteReducer = (store = initialState, action) => {
   case 'CREATE' : {
     return [...store, { content: action.content, id: getId(), votes:0 }]
   }
+  case 'INIT_ANECDOTES': {
+    return action.data
+  }
   default:
     return store
   }
 }
-
+export const anecdoteInitialization = (data) => {
+  return {
+    type: 'INIT_ANECDOTES',
+    data
+  }
+}
 export const anecdoteCreation = (content) => {
 
   return {
