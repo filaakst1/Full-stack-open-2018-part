33b0112ba27eaa954620d95d5a94cb42f34adb93
Tomi Laakso <tomi.j.laakso@gmail.com>
2018-11-11 18:15:53 +0200

    Storing filter value

--- redux-anecdotes-v2/src/components/Filter.js
+++ redux-anecdotes-v2/src/components/Filter.js
@@ -1,9 +1,24 @@
-
 import React from 'react'
+import PropTypes from 'prop-types'
+import { filterChange } from '../reducers/filterReducer'
 
 class Filter extends React.Component {
+  componentDidMount() {
+    const { store } = this.context
+    this.unsubscribe = store.subscribe(() =>
+      this.forceUpdate()
+    )
+  }
+
+  componentWillUnmount() {
+    this.unsubscribe()
+  }
+
+
   handleChange = (event) => {
-    // input-kentän arvo muuttujassa event.target.value
+    this.context.store.dispatch(
+      filterChange(event.target.value)
+    )
   }
   render() {
     const style = {
@@ -17,5 +32,7 @@ class Filter extends React.Component {
     )
   }
 }
-
+Filter.contextTypes = {
+  store: PropTypes.object
+}
 export default Filter
--- redux-anecdotes-v2/src/reducers/filterReducer.js
+++ redux-anecdotes-v2/src/reducers/filterReducer.js
@@ -3,8 +3,17 @@ const initialState=''
 const filterReducer = (store = initialState, action) => {
   console.log('FILTER ACTION: ', action)
   switch(action.type) {
+  case 'UPDATE_FILTER_VALUE': {
+    return action.filter
+  }
   default:
     return store
+  }
+}
+export const filterChange = (filter) => {
+  return {
+    type: 'UPDATE_FILTER_VALUE',
+    filter: filter
   }
 }
 export default filterReducer
