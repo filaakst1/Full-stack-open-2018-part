ccbc50b5cdacabdef16606970062c7fd534cd322
Tomi Laakso <tomi.j.laakso@gmail.com>
2018-11-14 22:46:55 +0200

    Dummy commit to change revision
    Dummy commit to change revision. Created because there exercise was
    completed during the previous exercise

--- redux-anecdotes-v2/src/components/AnecdoteList.js
+++ redux-anecdotes-v2/src/components/AnecdoteList.js
@@ -13,6 +13,7 @@ class AnecdoteList extends React.Component {
     this.props.voteAnecdote(anecdote)
     this.props.notify(`you voted '${anecdote.content}'`, 5)
   }
+  
   render() {
     const { anecdotesToShow } = this.props
     return (
