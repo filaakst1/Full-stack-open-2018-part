2ef2e2ed486adec18f1054524eda50d55710a449
Tomi Laakso <tomi.j.laakso@gmail.com>
2018-11-17 18:59:21 +0200

    Set about to use grid and display image of Hinton

--- routed-anecdotes/src/App.js
+++ routed-anecdotes/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { BrowserRouter as Router, Route, NavLink} from 'react-router-dom'
-import { ListGroup,ListGroupItem  } from 'react-bootstrap'
+import { ListGroup,ListGroupItem, Grid, Row, Col } from 'react-bootstrap'
+import image from './hinton.jpeg';
 const Menu = () => { 
   const style = {
     backgroundColor: 'LightBlue',
@@ -30,14 +31,21 @@ const AnecdoteList = ({ anecdotes }) => (
 )
 const About = () => (
   <div>
-    <h2>About anecdote app</h2>
-    <p>According to Wikipedia:</p>
-    
-    <em>An anecdote is a brief, revealing account of an individual person or an incident. 
-      Occasionally humorous, anecdotes differ from jokes because their primary purpose is not simply to provoke laughter but to reveal a truth more general than the brief tale itself, 
-      such as to characterize a person by delineating a specific quirk or trait, to communicate an abstract idea about a person, place, or thing through the concrete details of a short narrative. 
-      An anecdote is "a story with a point."</em>
-
+     <h2>About anecdote app</h2>
+    <Grid>
+      <Row className="show-grid">
+        <Col xs={12} md={8}>
+          <p>According to Wikipedia:</p>
+          <em>An anecdote is a brief, revealing account of an individual person or an incident. 
+            Occasionally humorous, anecdotes differ from jokes because their primary purpose is not simply to provoke laughter but to reveal a truth more general than the brief tale itself, 
+            such as to characterize a person by delineating a specific quirk or trait, to communicate an abstract idea about a person, place, or thing through the concrete details of a short narrative. 
+            An anecdote is "a story with a point."</em>
+        </Col>
+        <Col xs={6} md={4}>
+          <img src={image} alt='Geoffrey Hinton' width='250px'/>
+        </Col>
+      </Row>
+    </Grid>
     <p>Software engineering is full of excellent anecdotes, at this app you can find the best and add more.</p>
   </div>
 )
